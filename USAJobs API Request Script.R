e0ced9a1abc36a507f70592a000377831004f064
Andrew <andrew.s.cavalier@gmail.com>
2021-11-17 19:29:11 +0800

    Update USAJobs API Request Script.R
    Updated with initial R package installations

--- USAJobs API Request Script.R
+++ USAJobs API Request Script.R
@@ -1,4 +1,14 @@
 ##Step 1
+
+install.packages("httr")
+require("httr")
+
+install.packages("jsonlite")
+require("jsonlite")
+
+##Step 2 
+## Request must include 3 Parameters: Host, User Agent, Authorization Key
+
 request <- "request"
 host <- "data.usajobs.gov"
 useragent <- "andrew.s.cavalier@gmail.com"
@@ -9,12 +19,14 @@ base_url <- "https://data.usajobs.gov/api/search?PositionTitle=Psychologist"
 call1 <- paste(base_url)
 call1
 
-##Step 2
-get_jobs <- GET(call1,request,host,useragent,authkey, type = "basic")
+##Step 3
+
+get_jobs <- GET(call1, query=list(api_key=authkey,host,useragent))
 
 get_jobs
 
-##Step3
+##Step4
+
 get_jobs_text <-content(get_jobs, "text")
 
 get_jobs 
